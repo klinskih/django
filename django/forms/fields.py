76f762d939f87d46f74b7347c0ac7367d98d7395
root <root@localhost.localdomain>
2012-07-23 15:26:05 +0600

    Чек боксы работают с FB

--- django/forms/fields.py
+++ django/forms/fields.py
@@ -641,6 +641,21 @@ class BooleanField(Field):
         if not value and self.required:
             raise ValidationError(self.error_messages['required'])
         return value
+    #KAG
+    def prepare_value(self, value):
+	"""Returns a Python boolean object."""
+        # Explicitly check for the string 'False', which is what a hidden field
+        # will submit for False. Also check for '0', since this is what
+        # RadioSelect will provide. Because bool("True") == bool('1') == True,
+        # we don't need to handle that explicitly.
+        if isinstance(value, basestring) and value.lower() in ('false', '0'):
+            value = False
+        else:
+            value = bool(value)
+        value = super(BooleanField, self).to_python(value)
+        if not value and self.required:
+            raise ValidationError(self.error_messages['required'])
+        return value
 
 class NullBooleanField(BooleanField):
     """
